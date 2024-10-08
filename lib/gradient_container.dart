83c5b6a6d55037b563e4ad93e6ceb10613063e49
Angel Diaz <angel.diaz5609@alumnos.udg.mx>
2024-10-08 10:59:21 -0600

    first steps in Flutter

---
+++ lib/gradient_container.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const  endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer (this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  void rollDice()
+  {
+
+  }
+
+  @override
+  Widget build(context) {
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color1, color2],
+            begin: startAlignment,
+            end: endAlignment,
+          ),
+        ),
+        child: Center(
+          child: Column(children: [
+              Image.asset('assets/images/dice-2.png', width: 200
+              ),
+              TextButton(onPressed: rollDice,
+                child: const Text('Roll Dice'),)
+            ],
+          ), 
+        ),
+      );
+  }
+  /* @override
+  Widget build(context) {
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colors,
+            begin: startAlignment,
+            end: endAlignment,
+          ),
+        ),
+        child: const Center(
+          child: StyledText('Hello World :)!'),
+        ),
+      );
+  } */
+
+}
